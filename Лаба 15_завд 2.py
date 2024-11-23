5f56848fb1b9a895e724174953e38d90a80a0cbc
Kateryna Svetiukha <148003224+KaterynaSvetiukha@users.noreply.github.com>
2024-11-23 22:39:48 +0200

    Update Лаба 15_завд 2.py

--- Лаба 15_завд 2.py
+++ Лаба 15_завд 2.py
@@ -7,11 +7,14 @@ fixed_df = pd.read_csv('2013.csv',
                        index_col='Date')
 
 print(fixed_df.head())
+fixed_df.index = pd.to_datetime(fixed_df.index)
 
-monthly_totals = fixed_df.groupby('Date').sum()
-monthly_totals['Total'] = monthly_totals.sum(axis=1)
+fixed_df.fillna(0, inplace=True)
+fixed_df['Total'] = fixed_df.sum(axis=1)
+monthly_totals = fixed_df.resample('M').sum()
 most_popular_month = monthly_totals['Total'].idxmax()
-print(f"Найпопулярніший місяць у 2013 році: {most_popular_month}")
+print(f"Найпопулярніший місяць у 2013 році: {most_popular_month.strftime('%B')}")
 
 fixed_df.plot(figsize=(15, 10))
 plt.show()
+
